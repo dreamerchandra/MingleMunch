b625701e823ca1eb0f639755ef68435e4e89edfd
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2023-04-23 11:50:35 +0530

    added prettier and eslinter added miui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import MyApp from "./Button";
+import CssBaseline from "@mui/material/CssBaseline";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CssBaseline />
+    <MyApp />
+  </React.StrictMode>
+);
