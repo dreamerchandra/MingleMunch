64ab3bd353e93bf5bcb0b568f53508a52358290a
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2024-04-26 09:17:37 +0000

    chore: ui update

--- migrations/script/item.js
+++ migrations/script/item.js
@@ -13,13 +13,13 @@ const firebaseDb = getFirestore(app);
 const storage = getStorage(app);
 // 8056735175
 
+const availableCategory = ['XpozCHQdgE67lyWRAdax', 'hT0fz8Ke3z4LVwxFkQMR', 'D5P6q80F4LXiQVlu1wKA']
 const backupOrginalItem = async () => {
   const snap = await firebaseDb
     .collection('food')
     .where('shopId', '==', 'BOmEbao75ZSfXusKIOhi')
     .get();
   for (const doc of snap.docs) {
-    const data = doc.data();
     const id = doc.id;
     console.log(id);
     await firebaseDb.collection('food').doc(id).update({
