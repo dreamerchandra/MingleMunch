47f18e5485c6b0ba2e5fd61ae2e751d5b9de3000
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2024-05-04 10:38:06 +0530

    chore: updated phone number

--- src/web/module/LastOrder/LastOrder.tsx
+++ src/web/module/LastOrder/LastOrder.tsx
@@ -108,7 +108,7 @@ export const LastOrder: FC = () => {
         color: '#fff',
         borderRadius: 0,
       }}
-      href="tel:+918754791569"
+      href="tel:+918220080109"
     >
       {lastOrder?.status == 'pending' && <Text label="Pending At Hotel" />}
       {lastOrder?.status == 'ack_from_hotel' && (
