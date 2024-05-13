f7f3642a7773a367792e69af5e3947959072e611
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2024-05-13 14:01:10 +0530

    feat: analtyics wip

--- migrations/script/lta.js
+++ migrations/script/lta.js
@@ -14,7 +14,7 @@ const storage = getStorage(app);
 
 const report = async () => {
   const startDate = new Date();
-  startDate.setDate(startDate.getDate() - 3);
+  startDate.setDate(startDate.getDate() - 5);
   startDate.setHours(0, 0, 0, 0);
   const endDate = new Date();
   endDate.setDate(endDate.getDate() - 1);
