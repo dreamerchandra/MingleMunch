64d3c04b87e6a4607c03e4bcc36ccb2ad31289c9
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2024-05-15 12:35:31 +0530

    feat: updated nith bro validity to 50

--- functions/src/router/her-coupon.ts
+++ functions/src/router/her-coupon.ts
@@ -22,41 +22,42 @@ export const canProceedToApply = async (couponCode: string, userId: string) => {
     if (!coupon) {
       return {
         canProceed: false,
-        error: 'Invalid coupon',
-      }
+        error: 'Invalid coupon'
+      };
     }
     const { usedBy = [], ownerId } = coupon;
     if (ownerId === userId) {
       return {
         canProceed: false,
-        error: 'You cant use your own coupon',
-      }
+        error: 'You cant use your own coupon'
+      };
     }
-    if (usedBy.length >= 2) {
+    const isNithBro = couponCode === 'NITHBRO';
+    if (isNithBro ? usedBy.length >= 50 : usedBy.length >= 2) {
       return {
         canProceed: false,
-        error: 'Coupon Limit reached',
-      }
+        error: 'Coupon Limit reached'
+      };
     }
     const hasUsed = await firebaseDb
       .collection('herCoupon')
       .where('usedBy', 'array-contains', userId)
       .get();
-    if(!hasUsed.empty) {
+    if (!hasUsed.empty) {
       return {
         canProceed: false,
-        error: 'You have already used a coupon',
-      }
+        error: 'You have already used a coupon'
+      };
     }
     return {
-      canProceed: true,
+      canProceed: true
     };
   } catch (err) {
     console.log(err);
     return {
       canProceed: false,
-      error: 'Something went wrong',
-    }
+      error: 'Something went wrong'
+    };
   }
 };
 
@@ -88,8 +89,10 @@ export const canUseHerCoupon = async (req: Request, res: Response) => {
   return res.json({ data });
 };
 
-
-const checkCouponIfAlreadyExist = async (couponCode: string[], userId: string) => {
+const checkCouponIfAlreadyExist = async (
+  couponCode: string[],
+  userId: string
+) => {
   const couponRef = await firebaseDb
     .collection('herCoupon')
     .where('ownerId', '==', userId)
@@ -99,7 +102,7 @@ const checkCouponIfAlreadyExist = async (couponCode: string[], userId: string) =
     throw new Error('Coupon already exist');
   }
   for (const code of couponCode) {
-    const _code = code.split(' ').join('').toUpperCase()
+    const _code = code.split(' ').join('').toUpperCase();
     const couponRef = await firebaseDb.collection('herCoupon').doc(_code).get();
     logger.log(`Checking for ${_code} exist ${couponRef.exists}`);
     if (!couponRef.exists) {
@@ -108,22 +111,30 @@ const checkCouponIfAlreadyExist = async (couponCode: string[], userId: string) =
   }
   logger.log('No coupon available');
   throw new Error('No coupon available');
-}
+};
 
 const createMyHerCoupon = async (userId: string) => {
   const user = await firebaseAuth.getUser(userId);
-  if(!user) return null;
-  const name = user.displayName || user.phoneNumber?.split('+91')[1].slice(6,10) || 'THANKS';
-  const random5 = [name, ...Array(5).fill('').map(() => `${name}${Math.floor(Math.random() * 100)}`)]
+  if (!user) return null;
+  const name =
+    user.displayName ||
+    user.phoneNumber?.split('+91')[1].slice(6, 10) ||
+    'THANKS';
+  const random5 = [
+    name,
+    ...Array(5)
+      .fill('')
+      .map(() => `${name}${Math.floor(Math.random() * 100)}`)
+  ];
   logger.log(`Random5: ${random5}`);
   const couponCode = await checkCouponIfAlreadyExist(random5, userId);
   await firebaseDb.collection('herCoupon').doc(couponCode).set({
     ownerId: userId,
-    usedBy: [],
+    usedBy: []
   });
-  logger.log(`Coupon created for ${userId} is ${couponCode}`)
+  logger.log(`Coupon created for ${userId} is ${couponCode}`);
   return couponCode;
-}
+};
 export const getHerCoupon = async (req: Request, res: Response) => {
   const { uid: userId } = req.user;
   const couponRef = await firebaseDb
